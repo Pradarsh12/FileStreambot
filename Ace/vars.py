9b950ec53aecbd258967044465b39b583c1b790a
Pradarsh mittal <117892231+Pradrsh@users.noreply.github.com>
2023-07-01 17:23:44 +0530

    Update vars.py

--- Ace/vars.py
+++ Ace/vars.py
@@ -35,6 +35,6 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('mongodb+srv://xalesib814:yeshaiha@cluster0.m8yhmzl.mongodb.net/?retryWrites=true&w=majority'))
+    DATABASE_URL = str(getenv('DATABASE_URL'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
